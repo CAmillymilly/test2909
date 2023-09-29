eef11242e1fd7e41c90a5daf15fbda63c181ae83
CAmillymilly <130669887+CAmillymilly@users.noreply.github.com>
2023-09-29 14:21:55 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    // declaração de Variáveis
+    $numero = 4;
+    $nome = "Isis";
+
+    // lê a variável 
+    switch($numero) {
+
+        // compara a variável
+        case 4:
+            echo "O número é igual a 4 <br>";
+            break;
+        // compara a variável
+        case 5;
+            echo "O número é igual a 5 <br>";
+            break;
+        // valor mulo 
+        defauly:
+         echo "O número não corresponde aos valores <br>";
+
+        }
+          // lê a variável 
+    switch ($nome) {
+
+        // compara a variável 
+    case "Rosana":
+        echo "O nome é Rosana";
+        break;
+
+        // compara a variável 
+    case "Isis":
+        echo "O nome é Isis";
+        break;
+
+        // valor nulo
+    default:
+        echo "O nome não foi encontrado";
+}
